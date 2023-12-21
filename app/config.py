304f386f7b443a25cd1de264f678995211e8cb8b
saqib_mehmood <saqib.mehmood@tkxel.io>
2023-12-21 14:51:37 +0500

    databse config

--- app/config.py
+++ app/config.py
@@ -1,5 +1,12 @@
+import os
+
+# Access the environment variables
+db_username = os.environ.get('DB_USERNAME')
+db_password = os.environ.get('DB_PASSWORD')
+db_host = os.environ.get('DB_HOST')
+db_port = os.environ.get('DB_PORT')
+db_name = os.environ.get('DB_NAME')
+
 class Config:
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@host:port/database_name'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:OnSoncENALgI@qa-catchu-new.cayg4wndojlv.us-east-1.rds.amazonaws.com:5432/retail_analytics'
+    SQLALCHEMY_DATABASE_URI = f'postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # Other configurations if needed

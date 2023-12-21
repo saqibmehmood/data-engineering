f9183ccc76d03069f9f697c7297f2f4c1e31e841
saqib_mehmood <saqib.mehmood@tkxel.io>
2023-12-21 13:42:59 +0500

    ploty in requirements.txt file

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@host:port/database_name'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@host:port/database_name'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:OnSoncENALgI@qa-catchu-new.cayg4wndojlv.us-east-1.rds.amazonaws.com:5432/retail_analytics'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # Other configurations if needed

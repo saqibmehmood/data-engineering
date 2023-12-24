2a1aefa6a23cb77829198683b8214c6ca69927b2
saqib_mehmood <saqib.mehmood@tkxel.io>
2023-12-24 23:03:58 +0500

    pystest

--- tests.py
+++ tests.py
@@ -1,47 +1,8 @@
-import unittest
 import pytest
 from app import create_app
 from app.models import Transaction, db
 from datetime import datetime
 
-
-class TestApp(unittest.TestCase):
-
-    def setUp(self):
-        self.app = create_app().test_client()
-        self.app.testing = True
-
-    def test_top_products(self):
-        """
-        Test that top_products
-        :return:
-        """
-        response = self.app.get('/total_sales')
-        self.assertEqual(response.status_code, 200)
-        data = response.json  # Assuming your response is in JSON format
-        self.assertIsInstance(data, list)
-        if data:
-            for item in data:
-                self.assertIsInstance(item, dict)
-                self.assertIn('stockcode', item)
-                self.assertIn('description', item)
-                self.assertIn('total_quantity', item)
-
-    def test_avg_unit_price(self):
-        """
-        Test that avg_unit_price
-        :return:
-        """
-        response = self.app.get('/average_unit_price')
-        self.assertEqual(response.status_code, 200)
-        data = response.json
-        self.assertIsInstance(data, list)
-        if data:
-            for item in data:
-                self.assertIsInstance(item, dict)
-                self.assertIn('stockcode', item)
-                self.assertIn('description', item)
-                self.assertIn('avg_unit_price', item)
 
 
 @pytest.fixture
@@ -58,6 +19,8 @@ def client():
 
         with app.app_context():
             db.drop_all()  # Drop tables from the in-memory database after the test
+
+
 
 def test_calculate_total_sales(client):
     print("1")
@@ -101,23 +64,45 @@ def test_calculate_total_sales(client):
             assert expected['description'] == actual['description']
             assert expected['total_quantity'] == actual['total_quantity']
 
-    # print("2")
-    # # Call the API and validate the response
-    # response = client.get('/total_sales')
-    # data = response.get_json()
-    # print("3")
-    # print("###################################################")
-    # print("data: ", data)
-    # print("###################################################")
-    # # Assertions
-    # assert response.status_code == 200
-    # assert len(data) == 2
-    # assert {'stockcode': '12345', 'description': 'Test Product A', 'total_quantity': 15} in data
-    # assert {'stockcode': '67890', 'description': 'Test Product B', 'total_quantity': 3} in data
+@pytest.fixture
+def client_actual_db():
+    # Configure the Flask app for testing with the actual database
+    app = create_app()  # Assuming create_app() without args connects to the actual DB
+
+    with app.test_client() as client:
+        yield client
+
+def test_avg_unit_price(client_actual_db):
+    """
+    Test that avg_unit_price
+    """
+    response = client_actual_db.get('/average_unit_price')
+    assert response.status_code == 200
+    data = response.get_json()
+    assert isinstance(data, list)
+    for item in data:
+        assert isinstance(item, dict)
+        assert 'stockcode' in item
+        assert 'description' in item
+        assert 'avg_unit_price' in item
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_top_products(client_actual_db):
+    """
+    Test that top_products
+    """
+    response = client_actual_db.get('/total_sales')
+    assert response.status_code == 200
+    data = response.get_json()  # Use get_json() method
+    assert isinstance(data, list)
+    for item in data:
+        assert isinstance(item, dict)
+        assert 'stockcode' in item
+        assert 'description' in item
+        assert 'total_quantity' in item
+
+
+
 
 
 
